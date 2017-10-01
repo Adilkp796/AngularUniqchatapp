9b6ae1c862ad1b2bef5f22e01e898f4da8736e2e
Adil kp <dilu@m-ore.net>
2017-10-01 20:31:32 +0530

    first commit 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
@@ -11,11 +11,19 @@ import * as firebase from 'firebase/app';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewChecked {
   user: Observable<firebase.User>;
   items: FirebaseListObservable<any[]>;
   public msgVal: string;
+  public image:any;
+  public email:any;
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+  ngOnInit() {
+  }
 
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
   constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
     this.items = af.list('/messages', {
       query: {
@@ -25,21 +33,46 @@ export class AppComponent {
     this.user = this.afAuth.authState;
     console.log(afAuth);
   }
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
+
   currentUserDisplayName(): string {
     if (!this.afAuth.auth.currentUser.displayName) { return 'Guest' }
-    else { return this.afAuth.auth.currentUser.displayName}
+    else { return this.afAuth.auth.currentUser.displayName }
   }
   login() {
-   this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-}
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
 
-logout() {
+  logout() {
     this.afAuth.auth.signOut();
-}
+  }
 
-Send(desc: string) {
-    this.items.push({ message: desc,name:this.currentUserDisplayName()});
+  Send(desc: string) {
+    this.items.push({
+       message: desc,
+       name: this.currentUserDisplayName(),
+       image:this.afAuth.auth.currentUser.photoURL,
+       timestamp: Date.now(),
+       email:this.afAuth.auth.currentUser.email
+      });
     this.msgVal = '';
-}
-//whispering-mountain-33588.herokuapp.com/ | https://git.heroku.com/whispering-mountain-33588.git
+    this.email=this.afAuth.auth.currentUser.email;
+    // console.log(this.email);
+  }
+  isYou(email){
+     if(email==this.afAuth.auth.currentUser.email)
+     return true;
+    else
+      return false;
+  }
+  isMe(email){
+    if(email==this.afAuth.auth.currentUser.email)
+    return false;
+   else
+     return true;
+  }
 }
